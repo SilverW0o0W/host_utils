9e2c0318d71b2a022d305544d77e760a0546720e
SilverW0o0W <silver.codingcat@gmail.com>
2021-05-13 11:47:25 +0800

    Update install_python3.sh

--- shell/install_python3.sh
+++ shell/install_python3.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 yum install openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel
 wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
 tar -zxvf Python-3.7.9.tgz
-cd Python-3.7.9/
+cd Python-3.7.9/ || exit
 mkdir /usr/local/python3
 ./configure -prefix=/usr/local/python3
 make && make install
