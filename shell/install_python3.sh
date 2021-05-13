b49e2b2ffc60f59ce1e6632f5d5df6b290b8d429
SilverW0o0W <silver.codingcat@gmail.com>
2021-05-13 11:30:06 +0800

    Create install_python3.sh

---
+++ shell/install_python3.sh
@@ -0,0 +1,9 @@
+yum install openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel
+wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+tar -zxvf Python-3.7.9.tgz
+cd Python-3.7.9/
+mkdir /usr/local/python3
+./configure -prefix=/usr/local/python3
+make && make install
+ln -s /usr/local/python3/bin/python3 /usr/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
