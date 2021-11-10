9115cf2803b1becaba5355c8b9b92e985a981e66
SilverW0o0W <silver.codingcat@gmail.com>
2021-11-10 18:56:41 +0800

    Update install_mysql.sh

--- shell/install_CentOS/install_mysql.sh
+++ shell/install_CentOS/install_mysql.sh
@@ -3,6 +3,7 @@ wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
 yum -y update
 yum -y install mysql-server
+rm -f mysql-community-release-el7-5.noarch.rpm
 
 chown -R mysql:mysql /var/lib/mysql
 mysqld --initialize
